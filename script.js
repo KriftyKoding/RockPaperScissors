b34465dcf3e155295598b4c079cfae4b25c4d3fa
KrizzyDizzy <KaitlynMarieJones@gmail.com>
2021-04-06 11:18:08 -0700

    player round now calculates wins and displays. also changed paper photo so it is more obvious

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
 let compNumWins = 0;
-let yourNumWins = 0;
+let playerNumWins = 0;
+
+const playerWins = document.querySelector('#playerWins'); 
+playerWins.textContent = playerNumWins;
+
+const compWins = document.querySelector('#compWins'); 
+compWins.textContent = compNumWins;
+
 
 // listen for player choice
 document.getElementById("rockButton").addEventListener("click", () => playerRound("rock", compChoice()));
 document.getElementById("paperButton").addEventListener("click", () => playerRound("paper", compChoice()));
-document.getElementById("scissorsButton").addEventListener("click", () => playerRound("scissor", compChoice()));
+document.getElementById("scissorsButton").addEventListener("click", () => playerRound("scissors", compChoice()));
 
 //calc comp choice
 function compChoice() {
@@ -17,28 +24,34 @@ function playerRound(player, comp){
     console.log(player)
     console.log(comp)
     if (player == comp){
-    console.log("tie")
+        console.log("tie")
    }
    else if (player == "rock")
         if (comp == "paper"){
             console.log("comp wins")
+            compWins.textContent = compNumWins += 1;
         }
         else {
-         console.log("player wins")
+            console.log("player wins")
+            playerWins.textContent = playerNumWins += 1;
         }
     else if (player == "paper")
         if (comp == "scissors"){
             console.log("comp wins")
+            compWins.textContent = compNumWins += 1;
         }
         else {
             console.log("player wins")
+            playerWins.textContent = playerNumWins += 1;
         }
     else 
         if (comp == "rock"){
-        console.log("comp wins")
+            console.log("comp wins")
+            compWins.textContent = compNumWins += 1;
         }
         else {
             console.log("player wins")
+            playerWins.textContent = playerNumWins += 1;
             }
 }
 
