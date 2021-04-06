360938e5cd4a0166b3ea31b6bb90cf1b373304b5
KrizzyDizzy <KaitlynMarieJones@gmail.com>
2021-04-06 10:19:43 -0700

    add event listeners for button click and made sure playerround ws called correctly

--- script.js
+++ script.js
@@ -2,14 +2,21 @@
 let compNumWins = 0;
 let yourNumWins = 0;
 
+// listen for player choice
+document.getElementById("rockButton").addEventListener("click", () => playerRound("rock", compChoice()));
+document.getElementById("paperButton").addEventListener("click", () => playerRound("paper", compChoice()));
+document.getElementById("scissorsButton").addEventListener("click", () => playerRound("scissor", compChoice()));
 
+//calc comp choice
 function compChoice() {
-    //calc comp choice
     return  ["rock", "paper", "scissors"][Math.floor(Math.random()*3)];
   }
+
+// this is to see who wins and loses  
 function playerRound(player, comp){
-    // this is to see who wins and loses
-   if (player == comp){
+    console.log(player)
+    console.log(comp)
+    if (player == comp){
     console.log("tie")
    }
    else if (player == "rock")
